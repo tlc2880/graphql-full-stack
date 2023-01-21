8c115287231cfab9fde27f54cf1864259a155215
tlc2880 <tommylcao@gmail.com>
2023-01-21 07:20:43 -0800

    first commit

---
+++ server/FakeData.js
@@ -0,0 +1,181 @@
+// const UserList = [
+//   {
+//     id: 1,
+//     name: "Joe",
+//     username: "joe",
+//     age: 20,
+//     nationality: "USA",
+//     friends: [
+//       {
+//         id: 2,
+//         name: "James",
+//         username: "JamesTech",
+//         age: 20,
+//         nationality: "BRAZIL",
+//       },
+//       {
+//         id: 5,
+//         name: "Kelly",
+//         username: "kelly2023",
+//         age: 5,
+//         nationality: "CANADA",
+//       },
+//     ],
+//   },
+//   {
+//     id: 2,
+//     name: "Jerry",
+//     username: "JerryTLC",
+//     age: 20,
+//     nationality: "BRITAIN",
+//   },
+//   {
+//     id: 3,
+//     name: "Sarah",
+//     username: "cameron",
+//     age: 25,
+//     nationality: "USA",
+//     friends: [
+//       {
+//         id: 2,
+//         name: "Jeffrey",
+//         username: "JeffTech",
+//         age: 20,
+//         nationality: "MEXICO",
+//       },
+//     ],
+//   },
+//   {
+//     id: 4,
+//     name: "Rafe",
+//     username: "rafe123",
+//     age: 60,
+//     nationality: "GERMANY",
+//   },
+//   {
+//     id: 5,
+//     name: "Kelly",
+//     username: "kelly2019",
+//     age: 5,
+//     nationality: "CANADA",
+//   },
+// ];
+
+// const MovieList = [
+//   {
+//     id: 1,
+//     name: "Avengers Endgame",
+//     yearOfPublication: 2019,
+//     isInTheaters: true,
+//   },
+//   {
+//     id: 2,
+//     name: "Interstellar",
+//     yearOfPublication: 2007,
+//     isInTheaters: true,
+//   },
+//   {
+//     id: 3,
+//     name: "Superbad Dog",
+//     yearOfPublication: 2025,
+//     isInTheaters: false,
+//   },
+//   {
+//     id: 4,
+//     name: "Top Gun: Mavericd",
+//     yearOfPublication: 2022,
+//     isInTheaters: true,
+//   },
+// ];
+//////////////////////////////////
+const UserList = [
+  {
+      id: 1,
+      name: "Joe",
+      username: "joe",
+      age: 20,
+      nationality: "USA",
+      friends: [
+        {
+          id: 2,
+          name: "James",
+          username: "JamesTech",
+          age: 20,
+          nationality: "BRAZIL",
+        },
+        {
+          id: 5,
+          name: "Kelly",
+          username: "kelly2019",
+          age: 5,
+          nationality: "CANADA",
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: "James",
+      username: "JamesTech",
+      age: 20,
+      nationality: "BRAZIL",
+    },
+    {
+      id: 3,
+      name: "Sarah",
+      username: "cameron",
+      age: 25,
+      nationality: "MEXICO",
+      friends: [
+        {
+          id: 2,
+          name: "James",
+          username: "JamesTech",
+          age: 20,
+          nationality: "BRAZIL",
+        },
+      ],
+    },
+    {
+      id: 4,
+      name: "Rafe",
+      username: "rafe123",
+      age: 60,
+      nationality: "GERMANY",
+    },
+    {
+      id: 5,
+      name: "Kelly",
+      username: "kelly2019",
+      age: 5,
+      nationality: "CANADA",
+    },
+  ];
+  
+  const MovieList = [
+    {
+      id: 1,
+      name: "Avengers Endgame",
+      yearOfPublication: 2019,
+      isInTheaters: true,
+    },
+    {
+      id: 2,
+      name: "Interstellar",
+      yearOfPublication: 2007,
+      isInTheaters: true,
+    },
+    {
+      id: 3,
+      name: "Top Gun: Maverick",
+      yearOfPublication: 2022,
+      isInTheaters: true,
+    },
+    {
+      id: 4,
+      name: "JamesTech The Movie",
+      yearOfPublication: 2035,
+      isInTheaters: false,
+    },
+  ];
+
+module.exports = { UserList, MovieList };
